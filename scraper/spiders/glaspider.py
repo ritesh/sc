88a43d4dcfcf5b02f6f24eb2b37e963999cfc543
Ritesh Sinha <r@ritesh-sinha.com>
2011-06-15 17:52:48 +0100

    Image crawling for a single page works

--- scraper/spiders/glaspider.py
+++ scraper/spiders/glaspider.py
@@ -1,5 +1,7 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
+import re
+from scraper.items import MyImageItem
 
 class GlaSpider(BaseSpider):
     """A simple spider for gla.ac.uk"""
@@ -8,6 +10,17 @@ class GlaSpider(BaseSpider):
     start_urls = ["http://www.gla.ac.uk"]
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
+        items = []
         images = hxs.select('//img/@src').extract()
         for image in images:
-            print image
+            item = MyImageItem()
+            item['image_urls']=[]
+            if re.match('^http+', image):
+                imgurl = image
+            #If we have a relative url, append the site name to make it
+            #absolute
+            else:
+                imgurl = response.url + image
+            item['image_urls'].append(imgurl)
+            yield item
+
