3c631010568ef35039f68747265fedaa7521efcd
Ritesh Sinha <riteshkumarsinha@gmail.com>
2011-07-15 18:22:01 +0100

    Sqlite lite sample database

--- sqlite.sql
+++ sqlite.sql
@@ -1,33 +1,3 @@
-drop table if exists gla;
-drop table if exists gumtree;
-drop table if exists craigslist;
-create table gla(
-  id int(11) not null auto_increment,
-  url varchar(100) not null,
-  localpath varchar(500) not null,
-  checksum varchar(32) not null,
-  created int(11) not null comment "unix timestamp",
-  primary key(id),
-  key (url)
-) engine=innodb default charset=utf8;
-
-create table gumtree(
-  id int(11) not null auto_increment,
-  url varchar(100) not null,
-  localpath varchar(500) not null,
-  checksum varchar(32) not null,
-  created int(11) not null comment "unix timestamp",
-  primary key(id),
-  key (url)
-) engine=innodb default charset=utf8;
-create table craigslist(
-  
-  id int(11) not null auto_increment,
-  url varchar(100) not null,
-  localpath varchar(500) not null,
-  checksum varchar(32) not null,
-  created int(11) not null comment "unix timestamp",
-  primary key(id),
-  key (url)
-) engine=innodb default charset=utf8;
-
+create table gla(id integer primary key autoincrement, url varchar(100) not null, localpath varchar(500) not null, checksum varchar(32) not null, created integer not null);
+create table gumtree(id integer primary key autoincrement, url varchar(100) not null, localpath varchar(500) not null, checksum varchar(32) not null, created integer not null);
+create table craigslist(id integer primary key autoincrement, url varchar(100) not null, localpath varchar(500) not null, checksum varchar(32) not null, created integer not null);
