71d8466e410e84bf978a8e8f76130b8d365746b9
Ritesh Sinha <riteshkumarsinha@gmail.com>
2011-08-13 19:02:46 +0100

    Fixed the reconnection bug in adb.py Fix

--- adb.py
+++ adb.py
@@ -1,13 +1,16 @@
-from twisted.enterprise import adbapi
+#from twisted.enterprise import adbapi
+import reconpool
 from twisted.internet import reactor
 from subprocess import Popen, PIPE
+from twisted.python import log
 import re,string
 import time
 import optparse
-#sensitivity = 0.25 
-jphide = re.compile(r"(.*)jphide\([\*]{1,3}\)(.*)")
-outguess = re.compile(r"(.*)outguess(.*)\([\*]{1,3}\)")
-typeofsteg = "jo" #jphide and outguess
+jphide = re.compile(r"(.*)jphide\([\*]{3}\)(.*)")
+outguess = re.compile(r"(.*)outguess(.*)\([\*]{3}\)")
+jsteg = re.compile(r"(.*)jsteg(.*)\([\*]{3}\)")
+f5 = re.compile(r"(.*)f5(.*)\([\*]{3}\)")
+typeofsteg = "jofp" #jphide, jsteg, outguess and f5
 
 def getstuff(sensitivity, spidername):
     """docstring for getstuff"""
@@ -33,34 +36,50 @@ def runcmd(row, sensitivity):
         data = output.split()
         if re.match("^[negative|skipped]", data[2].strip()) is None:
             processdata(row[0], data[2], sensitivity)
-            print row[0], data[2]
     else:
-        print err
+        log.err(err)
 def processdata(row, data,sensitivity):
     return dbpool.runInteraction(__processdata, row, data,sensitivity)
 def __processdata(txn, row, data,sensitivity):
     res = convertnum(data)
     txn.execute(\
-            "INSERT INTO analysis(FK_data,sensitivity,positive,jsteg,outguess,jphide,invsecrets,f5,appended,created) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(row,sensitivity,res[0], res[1], res[2], res[3], res[4], res[5],res[6],time.time()) 
+            "SELECT * FROM analysis WHERE FK_data=%s AND sensitivity=%s", (row, sensitivity)
             )
-    print "Added to db!"
+    dbresult = txn.fetchone()
+    if dbresult:
+        print "Already added!"
+    else:
+        txn.execute(\
+                """INSERT INTO
+                analysis(FK_data,sensitivity,positive,jsteg,outguess,jphide,invsecrets,f5,appended,created)
+                VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                (row,sensitivity,res[0], res[1], res[2], res[3], res[4], res[5],res[6],time.time()) 
+                )
+        print "Added to db!"
 
 def convertnum(data):
     results = [0,0,0,0,0,0,0]
+    #results positive, jsteg, outguess, jphide, invsecrets, f5, appended
     print data
     if jphide.match(data):
-        results[3] = 1
+        results[3] = 3 
         results[0] = 1
     if outguess.match(data):
-        results[2] = 1
+        results[2] = 3
         results[0] = 1
+    if jsteg.match(data):
+        results[0] = 1
+        results[1] = 3
+    if f5.match(data):
+        results[0] = 1
+        results[5] = 3
     return results
 if __name__ == '__main__':
     opt = optparse.OptionParser()
     opt.add_option('--sensitivity', '-s')
     opt.add_option('--spidername', '-p')
     options, arguments = opt.parse_args()
-    dbpool = adbapi.ConnectionPool("MySQLdb", host='storo', user='ritesh', passwd='6510',db='ritesh' )
-    getstuff(options.spidername, options.sensitivity).addErrback(errhandler)
+    dbpool = reconpool.ReConnectionPool("MySQLdb", host='storo', user='ritesh', passwd='6510',db='ritesh' )
+    getstuff(options.sensitivity, options.spidername).addErrback(errhandler)
     reactor.run()
 
